ad1b377f7515a96dcf3acc68d5f557fbe1094c37
vm-simarpreet <simarpreet.singh@venuemonk.com>
2021-02-07 11:34:07 +0530

    stable

--- app.js
+++ app.js
@@ -31,10 +31,10 @@ app.use(require("express-session")({
 }));
 
 app.set('view engine','ejs');
-//
+
 app.use(passport.initialize());
 app.use(passport.session());
-// 
+
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -43,8 +43,7 @@ passport.deserializeUser(User.deserializeUser());
 app.get("/",function(req,res){
     res.render("signup");
 });
-//hello
-//hi2
+
 app.get("/home",isLoggedIn, function(req, res){
 let filter={};
 filter={
@@ -122,48 +121,7 @@ app.post("/username/:user/password/:pass/save",function(req,res){
         res.send("empty body");
     }
 
-   // for saving to database
-    // let c="";let dateMeet="";dateMeet+=new Date().toLocaleString("en-US");document.getElementsByClassName('uArJ5e UQuaGc kCyAyd QU4Gid foXzLb IeuGXd')[0].click(); for(var i=0;i<document.getElementsByClassName('GvcuGe')[0].childNodes.length;++i){c+=(document.getElementsByClassName('GvcuGe')[0].childNodes[i].innerText)+"\n";};function copyToClipboard(text) {
-    //     var dummy = document.createElement("textarea");
-    //     document.body.appendChild(dummy);
-    //      dummy.value = text;
-    //      dummy.select();
-    //      document.execCommand("copy");
-    //      document.body.removeChild(dummy);
-    //  };copyToClipboard(c);
-    // var form = document.createElement("FORM"); 
-    //     form.setAttribute("method", "post"); 
-    //     form.setAttribute("action", "http://localhost:2000/username/sam/password/sam/save"); 
-    //  form.innerHTML='<input type="hidden" name="date" value="'+dateMeet+'"/> <input type="hidden" name="data" value="'+c+'"/><input type="hidden" name="url" value="'+window.location.href+'" />'
-    // document.body.appendChild(form);
-    // form.submit();
-
- // for testing
-    //  let c="";let dateMeet="";dateMeet+=new Date().toLocaleString("en-US");document.getElementsByClassName('uArJ5e UQuaGc kCyAyd QU4Gid foXzLb IeuGXd')[0].click(); for(var i=0;i<document.getElementsByClassName('GvcuGe')[0].childNodes.length;++i){c+=(document.getElementsByClassName('GvcuGe')[0].childNodes[i].innerText)+"\n";};function copyToClipboard(text) {
-    //     var dummy = document.createElement("textarea");
-    //     document.body.appendChild(dummy);
-    //      dummy.value = text;
-    //      dummy.select();
-    //      document.execCommand("copy");
-    //      document.body.removeChild(dummy);
-    //  };
-    // var form = document.createElement("FORM"); 
-    //     form.setAttribute("method", "post"); 
-    //     form.setAttribute("action", "http://localhost:2000/username/sam/password/sam/save"); 
-    //  form.innerHTML='<input type="hidden" name="date" value="'+dateMeet+'"/> <input type="hidden" name="data" value="'+c+'"/><input type="hidden" name="url" value="'+window.location.href+'" />'
-    // document.body.appendChild(form);
-    // form.submit();
-
-    //for copy
-      // let c="";c+=new Date().toLocaleString("en-US");document.getElementsByClassName('uArJ5e UQuaGc kCyAyd QU4Gid foXzLb IeuGXd')[0].click();c+='\n'; for(var i=0;i<document.getElementsByClassName('GvcuGe')[0].childNodes.length;++i){c+=Number(i+1)+" "+document.getElementsByClassName('GvcuGe')[0].childNodes[i].innerText+'\n';};function copyToClipboard(text) {
-    //     var dummy = document.createElement("textarea");
-    //     document.body.appendChild(dummy);
-    //      dummy.value = text;
-    //      dummy.select();
-    //      document.execCommand("copy");
-    //      document.body.removeChild(dummy);
-    //  };copyToClipboard(c);
-    
+  
     
 
 });
@@ -171,11 +129,11 @@ app.post("/username/:user/password/:pass/save",function(req,res){
 
 
 //handling user sign up
-app.post("/register", function(req, res){
+app.post("/signup", function(req, res){
 User.register(new User({username:req.body.username}),req.body.password, function(err, user){
        if(err){
             console.log(err);
-            return res.render('register');
+            return res.render('signup');
         } //user stragety
         passport.authenticate("local")(req, res, function(){
             res.redirect("/home"); //once the user sign up
